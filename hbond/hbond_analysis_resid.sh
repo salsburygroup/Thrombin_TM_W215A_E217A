abe79e0d421b8b27071339f9836f826918e8c7be
Fred Salsbury <salsbufr@gemini.deac.wfu.edu>
2023-12-12 16:10:53 -0500

    merge fix

--- hbond/hbond_analysis_resid.sh
+++ hbond/hbond_analysis_resid.sh
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 python ../python/hbond_analysis_resid.py -s ../protein_common_atom.pdb -t ../WT_WTTM456_stride100_aligned.dcd -o hbond_stride100_WT_WTTM456
 python ../python/hbond_analysis_resid.py -s ../protein_common_atom.pdb -t ../WT_WE_stride100_aligned.dcd -o hbond_stride100_WT_WE
 python ../python/hbond_analysis_resid.py -s ../protein_common_atom.pdb -t ../WT_WETM456_stride100_aligned.dcd -o hbond_stride100_WT_WETM456
-=======
-python ~/python/hbond_analysis_resid.py -s ../protein_common_atom.pdb -t ../WT_WTTM456_stride100_aligned.dcd -o hbond_stride100_WT_WTTM456
-python ~/python/hbond_analysis_resid.py -s ../protein_common_atom.pdb -t ../WT_WE_stride100_aligned.dcd -o hbond_stride100_WT_WE
-python ~/python/hbond_analysis_resid.py -s ../protein_common_atom.pdb -t ../WT_WETM456_stride100_aligned.dcd -o hbond_stride100_WT_WETM456
->>>>>>> 05e619c11fb3fda267d2df7a27f05e08cd15ac95
-
 
